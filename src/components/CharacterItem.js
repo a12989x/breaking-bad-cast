e4b519f55da3c893cfe37d3a5ae473c747552dba
A12989x <alexis12989x@gmail.com>
2020-07-03 17:43:13 -0500

    import useState

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
-const CharacterItem = ({ img, name, portrayed, nickname, birthday, status }) => {
+const CharacterItem = ({
+    img,
+    name,
+    portrayed,
+    nickname,
+    birthday,
+    status,
+}) => {
     return (
         <div>
-            <div><img src={img} alt=""/></div>
-            <div><h2>{name}</h2>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h2>{name}</h2>
                 <ul>
-                    <li><b>Actor Name: </b>{portrayed}</li>
-                    <li><b>Nickname: </b>{nickname}</li>
-                    <li><b>Birthday: </b>{birthday}</li>
-                    <li><b>Status: </b>{status}</li>
+                    <li>
+                        <b>Actor Name: </b>
+                        {portrayed}
+                    </li>
+                    <li>
+                        <b>Nickname: </b>
+                        {nickname}
+                    </li>
+                    <li>
+                        <b>Birthday: </b>
+                        {birthday}
+                    </li>
+                    <li>
+                        <b>Status: </b>
+                        {status}
+                    </li>
                 </ul>
             </div>
         </div>
