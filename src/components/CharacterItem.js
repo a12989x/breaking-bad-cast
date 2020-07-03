61bd2b7551bfe2b6b0e3feac399fc2094f661b3b
A12989x <alexis12989x@gmail.com>
2020-07-03 18:04:12 -0500

    add classes to CharacterItem Component

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -9,30 +9,32 @@ const CharacterItem = ({
     status,
 }) => {
     return (
-        <div>
-            <div>
-                <img src={img} alt="" />
-            </div>
-            <div>
-                <h2>{name}</h2>
-                <ul>
-                    <li>
-                        <b>Actor Name: </b>
-                        {portrayed}
-                    </li>
-                    <li>
-                        <b>Nickname: </b>
-                        {nickname}
-                    </li>
-                    <li>
-                        <b>Birthday: </b>
-                        {birthday}
-                    </li>
-                    <li>
-                        <b>Status: </b>
-                        {status}
-                    </li>
-                </ul>
+        <div className="character">
+            <div className="character__inner">
+                <div className="character__front">
+                    <img src={img} alt="" className="character__image" />
+                </div>
+                <div className="character__back">
+                    <h2 className="character__name">{name}</h2>
+                    <ul>
+                        <li>
+                            <b>Actor Name: </b>
+                            {portrayed}
+                        </li>
+                        <li>
+                            <b>Nickname: </b>
+                            {nickname}
+                        </li>
+                        <li>
+                            <b>Birthday: </b>
+                            {birthday}
+                        </li>
+                        <li>
+                            <b>Status: </b>
+                            {status}
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
