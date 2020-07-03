dd2abb38e04ed0f14a760c4c6ac67fdf5a8ac44e
A12989x <alexis12989x@gmail.com>
2020-07-03 17:57:57 -0500

    add classes for Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import logo from '../img/logo.png';
 
 const Header = () => {
     return (
-        <header>
-            <img src={logo} alt="breaking bad logo"/>
+        <header className="header">
+            <img src={logo} alt="breaking bad logo" className="header__image" />
         </header>
     );
 };
