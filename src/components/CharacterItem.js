4127e6b6edc885211940cea2c849b7214a4cae71
A12989x <alexis12989x@gmail.com>
2020-07-03 17:09:30 -0500

    create CharacterItem Component

---
+++ src/components/CharacterItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterItem = () => {
+    return (
+        <div>
+            <p>Character Item Component</p>
+        </div>
+    );
+};
+
+export default CharacterItem;
