0a67760014cd2d8963ce0998978edf4e6eda0f1b
A12989x <alexis12989x@gmail.com>
2020-07-03 17:47:06 -0500

    setText depend on current value of input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 
 const Search = () => {
+    const [text, setText] = useState('');
+
     return (
         <form>
-            <input type="text" placeholder="Search characters" autoFocus />
+            <input
+                type="text"
+                placeholder="Search characters"
+                autoFocus
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
             <button type="submit">Search</button>
         </form>
     );
