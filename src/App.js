7a35fd66933220b60a1efab835532e2994d66ce9
A12989x <alexis12989x@gmail.com>
2020-07-03 16:54:05 -0500

    add initial state for App COmponent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 
 const App = () => {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     return (
         <div className="App">
             <Header />
