ded6bf82719a791b65577288dcb289f3e0368d61
A12989x <alexis12989x@gmail.com>
2020-07-04 10:07:53 -0500

    Updates

---
+++ precache-manifest.da92f45a39211419446eabde4e5ca9c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ec8f9b8e46b68ea4b0097a9c4feafe",
+    "url": "/breaking-bad-cast/index.html"
+  },
+  {
+    "revision": "f6a6a6ac40723eba76a7",
+    "url": "/breaking-bad-cast/static/css/main.dcc14530.chunk.css"
+  },
+  {
+    "revision": "c830348588edc99450e0",
+    "url": "/breaking-bad-cast/static/js/2.257b8084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-cast/static/js/2.257b8084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a6a6ac40723eba76a7",
+    "url": "/breaking-bad-cast/static/js/main.3241d1d5.chunk.js"
+  },
+  {
+    "revision": "aceb973de7feae88a752",
+    "url": "/breaking-bad-cast/static/js/runtime-main.039fb2eb.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-cast/static/media/logo.3795f66d.png"
+  }
+]);
